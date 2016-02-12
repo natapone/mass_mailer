c05e687056a7f2a064501143c32e293d1a1b52d5
Natapone C <natapone@gmail.com>
2016-02-12 11:32:56 +0700

    read mail from excal and return as hash

--- project/lib/mass/Mails.pm
+++ project/lib/mass/Mails.pm
@@ -12,59 +12,70 @@ has 'filename' => (
     builder => '_build_filename',
 );
 
+has 'key_field' => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_key_field',
+);
+
 sub _build_filename {
     my ( $self ) = @_;
 
     return 'data/mail/pro_guide.xlsx';
 }
 
+sub _build_key_field {
+    my ( $self ) = @_;
+
+    return 'EMAIL';
+}
+
 sub BUILD {
     ( my $self ) = @_;
 
     my $mail_header;
+    my $mail_list;
 
     # read data from csv file
     my $excel = Spreadsheet::XLSX->new($self->filename);
     my $sheet = $excel->{Worksheet}->[0]; # read only first sheet
 
-    printf(".............Sheet name : %s\n", $sheet->{Name});
+    # printf(".............Sheet name : %s\n", $sheet->{Name});
 
     # loop read row
     my $row_max =  $sheet->{MaxRow};
     my $row_min =  $sheet->{MinRow};
+
     print "Reading $row_max rows \n";
     foreach my $row ($row_min .. $row_max) {
-        # $sheet -> {MaxCol} ||= $sheet -> {MinCol};
-
+        my $mail_detail;
         foreach my $col ($sheet->{MinCol} ..  $sheet->{MaxCol}) {
             my $cell = $sheet->{Cells} [$row] [$col];
+
             if ($cell) {
-                printf("( %s , %s ) => %s\n", $row, $col, $cell -> {Val});
+                # printf("( %s , %s ) => %s\n", $row, $col, $cell -> {Val});
                 # first row is column name
                 if ($row == 0) {
                     $mail_header->{$col} = $cell->{Val};
+                } else {
+                    $mail_detail->{ $mail_header->{$col} } = $cell->{Val};
                 }
             }
-
-
         }
-print "header ....", Dumper($mail_header), "\n";
-        exit;
-        # first row is column name
-        # if (%mail_hash) {
-        #     $sheet->{MaxCol} ||= $sheet -> {MinCol};
-        # }
 
+        # add data to mail list
+        # check duplicate from main key
+        if ( $mail_list->{$mail_detail->{$self->key_field}} ) {
+            print "Duplicate mail: ", Dumper($mail_detail), "\n";
+        } else {
+            $mail_list->{$mail_detail->{$self->key_field}} = $mail_detail;
+        }
     }
+    # print "list ....", Dumper($mail_list), "\n";
+    # print "header ....", Dumper($mail_header), "\n";
 
-
-
-    return $excel;
+    return $mail_list;
 }
-
-sub list {
-
-}
-
 
 1;
